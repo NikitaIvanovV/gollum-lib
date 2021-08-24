b16e236dccc02e43af5512553debe25e46b4aa98
ViChyavIn <nikita.vyach.ivanov@gmail.com>
2021-08-25 00:13:28 +0500

    Fix: media macros don't work with set base_path

--- lib/gollum-lib/macro/audio.rb
+++ lib/gollum-lib/macro/audio.rb
@@ -2,6 +2,7 @@ module Gollum
   class Macro
     class Audio < Gollum::Macro
       def render (fname)
+        fname = @wiki.base_path + '/' + fname unless @wiki.base_path.empty?
         "<audio width=\"100%\" height=\"100%\" src=\"#{fname}\" controls=\"\"> HTML5 audio is not supported on this Browser.</audio>"
       end
     end
--- lib/gollum-lib/macro/video.rb
+++ lib/gollum-lib/macro/video.rb
@@ -2,6 +2,7 @@ module Gollum
   class Macro
     class Video < Gollum::Macro
       def render (fname)
+        fname = @wiki.base_path + '/' + fname unless @wiki.base_path.empty?
         "<video width=\"100%\" height=\"100%\" src=\"#{fname}\" controls=\"\"> HTML5 video is not supported on this Browser.</video>"
       end
     end
